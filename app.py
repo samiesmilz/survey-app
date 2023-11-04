0884f703dbc5967fabac21f7a7b59a0ed3c4550c
samiesmilz <samiesmilz@gmail.com>
2023-11-04 19:11:06 +0800

    Storing responses in session

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import surveys
-from flask import Flask, request, flash, render_template, redirect, session
+from flask import Flask, request, flash, render_template, redirect, session, make_response
 app = Flask(__name__, static_folder="static")
 app.config["SECRET_KEY"] = "samie"
 
@@ -25,6 +25,7 @@ def show_questions(id):
   global num_of_survey_questions
   survey = request.form["survey"]
   num_of_survey_questions = len(survey_types.get(survey, "None").questions)
+  session["user_responses"] = []
 
   if id < num_of_survey_questions:
       title = survey_types.get(survey, "New").title
@@ -53,6 +54,7 @@ def show_question(id):
         return render_template("questions.html", survey=title, question=question, question_id=current_question)
     else:
         next_url = f"/questions/{current_question}"
+        flash("Invalid URL: Please answer the current question!")
         return redirect(next_url)
 
 @app.route("/answer", methods=["POST"])
@@ -63,6 +65,13 @@ def get_answer():
     answered_qsn = survey_types.get(survey, "New").questions[current_question].question
     answer = request.form["answer"]
     responses.append(answer)
+
+    # Add to session
+    user_respo = session["user_responses"]
+    user_respo.append(answer)
+    session["user_reponses"] = user_respo
+
+    # Add to the dict
     response_dict[answered_qsn] = answer
     current_question += 1
     session["current_question"] = current_question
@@ -82,3 +91,16 @@ def quiz_done():
   all_questions = survey_types.get(survey, "New").questions
 
   return render_template("done.html", questions = all_questions, response_dict = response_dict)
+
+# Working with coookies
+@app.route("/cookie-form")
+def show_cookie_form():
+   return render_template("cookie-form.html")
+
+@app.route("/cookies")
+def make_cookies():
+   fav_color = request.args["cookie_name"]
+   html = render_template("show-cookies.html", fav_color=fav_color)
+   res = make_response(html)
+   res.set_cookie("fav_color", fav_color)
+   return res
